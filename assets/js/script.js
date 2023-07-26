318fe492c4b42edd46a01c04d5c0601ce1dc68b8
Nick Burr <nicholasstclairburr@gmail.com>
2023-07-26 13:39:14 -0600

    City

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const apiKey = `40c5846d93a5d9298ee752b2e4b83b4f`;
+var city;
+city = document.querySelector('#cityInput')
+console.log(city)
+// var geoCodeURL =`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+
+// // this below is going to have to be changed to have lat long 
+// var queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`
+
+// console.log(queryUrl);
+// fetch(queryUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
