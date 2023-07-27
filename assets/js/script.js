793995ec2d01d62c21fca7626fd1bbf02fb8f40c
Nick Burr <nicholasstclairburr@gmail.com>
2023-07-27 00:39:24 -0600

    working current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,61 @@
 const apiKey = `40c5846d93a5d9298ee752b2e4b83b4f`;
 var city = document.querySelector('#cityInput') // need a value?
 var searchButton = document.querySelector('#search')
-console.log("🚀 ~ file: script.js:4 ~ searchButton:", searchButton)
-var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=imperial`
-var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=${apiKey}&units=imperial`
+var insertCity = document.querySelector('#insertCity')
+var todaysConditions = document.querySelector('#conditions')
+var todaysTemp = document.querySelector('#todaysTemp')
+var todaysWind = document.querySelector('#todaysWind')
+var todaysHumidity = document.querySelector('#todaysHumidity')
+// window.addEventListener('load', function(){
+//     this.localStorage.getItem('')
+// })
 
 searchButton.addEventListener('click', function(event){
     event.preventDefault()
-    console.log(event)
-    console.log(city.value)
-    
-    console.log(currentWeatherURL);
-        fetch(currentWeatherURL)
+    var citySearch = city.value
+    insertCity.textContent = `Today's weather in ${citySearch}`
+    var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=imperial`
+
+    var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&appid=${apiKey}&units=imperial`
+
+    fetch(currentWeatherURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-        });
-    fetch(fiveDayURL)
-        .then(function (response){
-            console.log(response);
-            return response.json();
+            // var icon = data.weather[0].icon
+            // todaysConditions.content = '\'+`${icon}`
+            todaysConditions.textContent = `${data.weather[0].main}`
+            todaysTemp.textContent = `${data.main.temp}\xB0 F`
+            todaysWind.textContent = `windspeed: ${data.wind.speed}mph`
+            todaysHumidity.textContent = `${data.main.humidity}% relative humidity`
+            
         })
-        .then(function(promise){
-            console.log(promise)
-        })
+        .catch(function(error){ 
+            alert("City not found!")
+    });
+           
+    
+    
+   
+    
+    
+    // temp.textContent = `Temp: ${}response.main`
+        
+
+
+        
+        
+
+
+    // fetch(fiveDayURL)
+    //     .then(function (response){
+    //         return response.json();
+    //     })
+    //     .then(function(promise){
+    //         console.log(promise)
+    //     })
 
     })
 
