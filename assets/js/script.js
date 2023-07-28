8c5bcd744d7db340295ad0b05f6dc0e77833746e
Nick Burr <nicholasstclairburr@gmail.com>
2023-07-28 13:50:11 -0600

    Oops, forgot to commit yesterday!

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var todaysConditions = document.querySelector('#conditions')
 var todaysTemp = document.querySelector('#todaysTemp')
 var todaysWind = document.querySelector('#todaysWind')
 var todaysHumidity = document.querySelector('#todaysHumidity')
+var icon = document.getElementById('icon')
+
 // window.addEventListener('load', function(){
 //     this.localStorage.getItem('')
 // })
@@ -13,6 +15,7 @@ var todaysHumidity = document.querySelector('#todaysHumidity')
 searchButton.addEventListener('click', function(event){
     event.preventDefault()
     var citySearch = city.value
+    // localStorage.setItem(key, value)
     insertCity.textContent = `Today's weather in ${citySearch}`
     var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=imperial`
 
@@ -24,6 +27,7 @@ searchButton.addEventListener('click', function(event){
         })
         .then(function (data) {
             console.log(data);
+            icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
             todaysConditions.textContent = `${data.weather[0].main}`
             todaysTemp.textContent = `${data.main.temp}\xB0 F`
             todaysWind.textContent = `windspeed: ${data.wind.speed}mph`
@@ -39,7 +43,6 @@ searchButton.addEventListener('click', function(event){
    
     
     
-    // temp.textContent = `Temp: ${}response.main`
         
 
 
@@ -54,10 +57,6 @@ searchButton.addEventListener('click', function(event){
         .then(function(data){
             console.log(data)
             for (let index = 7; index < 46; index += 8) {
-                var indexString = index.toString()
-                console.log("type:" + typeof indexString + " indexString:" + indexString)
-                // var targetCard = document.querySelector(`#${indexString}`)
-                // console.log("Target card:"+ targetCard)
                 var date = data.list[index].dt_txt;
                 console.log(date);
                 var conditions = data.list[index].weather[0].main;
@@ -68,6 +67,32 @@ searchButton.addEventListener('click', function(event){
                 console.log(`wind: ${wind}mph`);
                 var humidity = data.list[index].main.humidity;
                 console.log(`${humidity}% rh`);
+                var cardContainer = document.getElementById("forecast")
+                var card = document.createElement('div')
+                card.classList.add('card','col-6','col-md-2')
+                cardContainer.append(card)
+                var cardBody = document.createElement('div')
+                cardBody.classList.add('card-body')
+                card.append(cardBody)
+                var cardTitle = document.createElement('h5')
+                cardTitle.classList.add('card-title')
+                cardTitle.textContent = date
+                cardBody.append(cardTitle)
+                var cardText = document.createElement('p')
+                cardText.classList.add('card-text')
+                cardText.textContent = `${conditions}\n${temp}\xB0 F\nwind: ${wind}mph\n${humidity}% rh`
+                cardBody.append(cardText)
+
+
+           
+
+
+
+
+
+
+
+           
             }
             
         })
