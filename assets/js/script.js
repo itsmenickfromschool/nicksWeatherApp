a268c64ac75fa863fdadf7ffde93e51c99b4f6ea
Nick Burr <nicholasstclairburr@gmail.com>
2023-07-29 00:54:43 -0600

    added localStorage set, still need GET

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,95 +8,88 @@ var todaysWind = document.querySelector('#todaysWind')
 var todaysHumidity = document.querySelector('#todaysHumidity')
 var icon = document.getElementById('icon')
 var cardContainer = document.getElementById("forecast")
-// window.addEventListener('load', function(){
-//     this.localStorage.getItem('')
-// })
 var cityArray = []
+var buttonDiv = document.getElementById('buttonDiv')
+
 searchButton.addEventListener('click', function(event){
     event.preventDefault()
     var citySearch = city.value
-    cityArray.unshift(citySearch)
-    localStorage.setItem("search", cityArray)
-    insertCity.textContent = `Today's weather in ${citySearch}`
-    var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=imperial`
     
-    var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&appid=${apiKey}&units=imperial`
+    insertCity.textContent = `Today's weather in ${citySearch}`;
+    var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=imperial`;
+    
+    var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&appid=${apiKey}&units=imperial`;
     
     fetch(currentWeatherURL)
-    .then(function (response) {
-        return response.json();
-        
-    })
-    .then(function (data) {
-        console.log(data);
-        icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
-        todaysConditions.textContent = `${data.weather[0].main}`
-        todaysTemp.textContent = `${data.main.temp}\xB0 F`
-        todaysWind.textContent = `windspeed: ${data.wind.speed}mph`
-        todaysHumidity.textContent = `${data.main.humidity}% relative humidity`
-        
-    })
-    .catch(function(error){ 
-        alert("City not found!")
-    });
-         
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        .then(function (response) {
+            if (response.status === 200) {
+                cityArray.unshift(citySearch);
+                localStorage.setItem("search", cityArray);
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+            todaysConditions.textContent = `${data.weather[0].main}`;
+            todaysTemp.textContent = `${data.main.temp}\xB0 F`;
+            todaysWind.textContent = `windspeed: ${data.wind.speed}mph`;
+            todaysHumidity.textContent = `${data.main.humidity}% relative humidity`; 
+        })
+        .catch(function(error){ 
+            alert("City not found!");
+    });  
     fetch(fiveDayURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        cardContainer.innerHTML = ''
-        for (let index = 7; index < 46; index += 8) {
-            var date = data.list[index].dt_txt;
-            console.log(date);
-            var icon = `http://openweathermap.org/img/w/${data.list[index].weather[0].icon}.png`
-            var conditions = data.list[index].weather[0].main;
-            console.log(conditions);
-            var temp = data.list[index].main.temp;
-            console.log(`${temp}\xB0 F`);
-            var wind = data.list[index].wind.speed;
-            console.log(`wind: ${wind}mph`);
-            var humidity = data.list[index].main.humidity;
-            console.log(`${humidity}% rh`);
-            var card = document.createElement('div')
-            card.classList.add('card','col-6','col-md-2','bg-dark', 'text-light')
-            cardContainer.append(card)
-            var cardBody = document.createElement('div')
-            cardBody.classList.add('card-body')
-            card.append(cardBody)
-            var cardTitle = document.createElement('h5')
-            cardTitle.classList.add('card-title')
-            cardTitle.textContent = dayjs(date).format('ddd MMMM D');
-            cardBody.append(cardTitle)
-            var cardIcon = document.createElement('img')
-            cardIcon.src = icon;
-            cardBody.append(cardIcon)
-            var cardText = document.createElement('p')
-            cardText.classList.add('card-text')
-            cardText.textContent = `${conditions}\n${temp}\xB0 F\nwind: ${wind}mph\n${humidity}% rh`
-            cardBody.append(cardText)
-        }
-        
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            cardContainer.innerHTML = ''
+            for (let index = 7; index < 46; index += 8) {
+                var date = data.list[index].dt_txt;
+                var icon2 = `http://openweathermap.org/img/w/${data.list[index].weather[0].icon}.png`;
+                var conditions = data.list[index].weather[0].main;
+                var temp = data.list[index].main.temp;
+                var wind = data.list[index].wind.speed;
+                var humidity = data.list[index].main.humidity;
+                var card = document.createElement('div');
+                card.classList.add('card','col-12','col-sm-3','col-md-2','bg-dark', 'text-light');
+                cardContainer.append(card);
+                var cardBody = document.createElement('div');
+                cardBody.classList.add('card-body');
+                card.append(cardBody);
+                var cardTitle = document.createElement('h5');
+                cardTitle.classList.add('card-title');
+                cardTitle.textContent = dayjs(date).format('ddd MMMM D');
+                cardBody.append(cardTitle);
+                var cardIcon = document.createElement('img');
+                cardIcon.src = icon2;
+                cardBody.append(cardIcon);
+                var condies = document.createElement('p');
+                condies.classList.add('card-text');
+                condies.textContent = `${conditions}`;
+                cardBody.append(condies);
+                var tempText = document.createElement('p');
+                tempText.classList.add('card-text');
+                tempText.textContent = `${temp}\xB0 F`;
+                cardBody.append(tempText);
+                var windy = document.createElement('p');
+                windy.classList.add('card-text');
+                windy.textContent = `wind: ${wind}mph`;
+                cardBody.append(windy);
+                var humid = document.createElement('p');
+                humid.classList.add('card-text');
+                humid.textContent = `${humidity}% rh`;
+                cardBody.append(humid);
+            }
     })
 
 
 })
 /* TODO *******************
 -add icons in cards for 5 day DONE
--add search to local storage
+-add search to local storage DONE
 -create buttons for history
 -get localstorage on the page load and also on the click event
 -remove cards when new search happens. DONE
